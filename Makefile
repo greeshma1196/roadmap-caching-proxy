4aae4b1734bf3ac7e2f386b963da7ca4ef105dea
Greeshma Ann Mathew <greeshma.mathew@gmail.com>
2025-02-03 10:58:42 +0100

    implemented handler to fetch products data

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 build:
 	go build -o caching-proxy
+run:
+	./caching-proxy --port 3000 --origin http://localhost:8080/products
 test:
 	go test ./... -coverprofile cover.out -v
 lint:
--- main.go
+++ main.go
@@ -1,16 +1,68 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
 )
+
+type product struct {
+	ID       int     `json:"id"`
+	Title    string  `json:"title"`
+	Category string  `json:"category"`
+	Price    float32 `json:"price"`
+}
+
+type products struct {
+	Products []product `json:"products"`
+}
 
 func main() {
 
-	portPtr := flag.Int("port", 8080, "an int")
-	originPtr := flag.String("origin", "http://dummyjson.com", "a string")
+	_ = flag.Int("port", 8080, "an int")
+	urlPtr := flag.String("origin", "http://dummyjson.com", "a string")
 	flag.Parse()
 
-	fmt.Printf("port: %d\n", *portPtr)
-	fmt.Printf("origin: %s\n", *originPtr)
+	urlStr := *urlPtr
+
+	path := parseURL(urlStr)
+
+	if path == "/products" {
+		prods := fetchProductsDataHandler()
+		fmt.Printf("Products: %+v\n", prods)
+	}
+
+}
+
+func parseURL(urlStr string) string {
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return u.Path
+}
+
+func fetchProductsDataHandler() products {
+	var products products
+	url := "http://dummyjson.com/products"
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer resp.Body.Close()
+
+	err = json.Unmarshal(body, &products)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return products
 }
